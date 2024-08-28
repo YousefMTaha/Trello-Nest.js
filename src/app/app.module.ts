97c88289641d25cee431621c51dca9d34551f992
yousef <yousefmahmoudmahmed@outlook.com>
2024-08-28 17:14:48 +0300

    change location files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { taskModule } from './task/task.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { userModule } from './user/user.module';
+import { taskModule } from 'src/task/task.module';
+import { userModule } from 'src/user/user.module';
 
 @Module({
   imports: [
