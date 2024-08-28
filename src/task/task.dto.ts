f419496f597afb734d0899c65a36d349c1f7e11d
yousef <yousefmahmoudmahmed@outlook.com>
2024-08-28 16:28:42 +0300

    modify type of assignTo

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,20 +1,20 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 import { Types } from 'mongoose';
 import { IsObjectId } from 'src/decorators/isObjectId.decorator';
 import { PartialType } from '@nestjs/mapped-types';
 import { IsFutureDate } from 'src/decorators/deadline.decorator';
-export class addTaskDto {
+export class AddTaskDto {
   @IsString()
   content: string;
-  
+
   @IsString()
   title: string;
 
   @IsObjectId()
-  assignTo: Types.ObjectId;
+  assignTo: [Types.ObjectId] | Types.ObjectId;
 
   @IsFutureDate()
   deadline: Date;
 }
 
-export class updateTaskDTO extends PartialType(addTaskDto) {}
+export class UpdateTaskDTO extends PartialType(AddTaskDto) {}
