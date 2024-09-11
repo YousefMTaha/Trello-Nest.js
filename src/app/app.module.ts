5775417c6180b647b335a574ecd9ed44bc327a8d
yousef <yousefmahmoudmahmed@outlook.com>
2024-09-11 16:56:39 +0300

    add configure module to access the .env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { taskModule } from 'src/task/task.module';
 import { userModule } from 'src/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: './config/.env', isGlobal: true }),
     MongooseModule.forRoot('mongodb://localhost:27017/trello-nest'),
     taskModule,
-    userModule
+    userModule,
   ],
   controllers: [AppController],
   providers: [AppService],
