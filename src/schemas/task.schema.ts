caec556ff853949724ba4028b186de2e532e6504
yousef <yousefmahmoudmahmed@outlook.com>
2024-09-11 16:59:30 +0300

    implement hook to automation delete the files in the cloud - modify attachments prop

--- src/schemas/task.schema.ts
+++ src/schemas/task.schema.ts
@@ -1,6 +1,15 @@
-import { MongooseModule, Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import {
+  MongooseModule,
+  Prop,
+  raw,
+  Schema,
+  SchemaFactory,
+} from '@nestjs/mongoose';
 import { Types } from 'mongoose';
 import { User } from './user.schema';
+import { CloudinaryService } from 'src/utils/services/cloudinary/cloudinary.service';
+import { CloudinaryModule } from 'src/utils/services/cloudinary/cloudinary.module';
+import { cloudinaryFolderPath } from 'src/utils/sysConstants';
 
 @Schema({ timestamps: true, discriminatorKey: 'type' })
 export class Task {
@@ -17,8 +26,13 @@ export class Task {
   @Prop({ type: Types.ObjectId, ref: 'User', required: true })
   createdBy: User;
 
-  @Prop({ min: Date.now(), required: false })
+  @Prop({ min: Date.now(), required: true })
   deadline: Date;
+
+  @Prop({
+    type: [{ public_id: String, secure_url: String }],
+  })
+  attachments: { public_id: string; secure_url: string }[];
 }
 
 @Schema()
@@ -35,13 +49,35 @@ export class ManyTask {
 
 const taskSchema = SchemaFactory.createForClass(Task);
 
-export const taskModel = MongooseModule.forFeature([
+export const taskModel = MongooseModule.forFeatureAsync([
   {
     name: Task.name,
-    schema: taskSchema,
+    useFactory: (CloudinaryService: CloudinaryService) => {
+      taskSchema.post('findOneAndDelete', async function (doc) {
+        if (doc?.attachments.length) {
+          await CloudinaryService.deleteAllFiles(
+            `${cloudinaryFolderPath}/${doc._id.toString()}`,
+          );
+        }
+
+        // next();
+      });
+      return taskSchema;
+    },
+    inject: [CloudinaryService],
+    imports: [CloudinaryModule],
     discriminators: [
       { name: OneTask.name, schema: SchemaFactory.createForClass(OneTask) },
       { name: ManyTask.name, schema: SchemaFactory.createForClass(ManyTask) },
     ],
   },
 ]);
+export type TtaskModel = Document & Task & { type: string };
+
+export type TOneTaskModel = TtaskModel & {
+  assignTo: Types.ObjectId;
+};
+
+export type TManyTaskModel = TtaskModel & {
+  assignTo: Types.ObjectId[];
+};
