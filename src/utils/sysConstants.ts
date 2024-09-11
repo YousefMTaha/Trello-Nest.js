ba362d1999d5c61fb1fa709e3462d7053cee1e5f
yousef <yousefmahmoudmahmed@outlook.com>
2024-09-11 17:05:15 +0300

    add some constants related to system

--- src/utils/sysConstants.ts
+++ src/utils/sysConstants.ts
@@ -11,3 +11,25 @@ export const taskTypes: { manyTask: string; oneTask: string } = {
   manyTask: 'ManyTask',
   oneTask: 'OneTask',
 };
+
+type TfileTypes = {
+  image: string;
+  text: string;
+  audio: string;
+  video: string;
+  application: string;
+  font: string;
+  multipart: string;
+};
+
+export const fileTypes: TfileTypes = {
+  image: 'image',
+  text: 'text',
+  audio: 'audio',
+  video: 'video',
+  application: 'application',
+  font: 'font',
+  multipart: 'multipart',
+};
+
+export const cloudinaryFolderPath: string = 'TrelloNest/Task';
