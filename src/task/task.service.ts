eb7e20cb784fe8fcdd632ef5fbd6e2482a7d38ba
yousef <yousefmahmoudmahmed@outlook.com>
2024-09-11 17:03:56 +0300

    implement some gets APIs - upload - delete attachments

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -8,11 +8,19 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { ManyTask, OneTask, Task } from 'src/schemas/task.schema';
+import {
+  ManyTask,
+  OneTask,
+  Task,
+  TManyTaskModel,
+  TOneTaskModel,
+  TtaskModel,
+} from 'src/schemas/task.schema';
 import { AddTaskDto, UpdateTaskDTO } from './task.dto';
 import { User } from 'src/schemas/user.schema';
-import { taskTypes } from 'src/utils/sysConstants';
+import { cloudinaryFolderPath, taskTypes } from 'src/utils/sysConstants';
 import { ObjectIdDTO } from 'src/common/common.dto';
+import { CloudinaryService } from 'src/utils/services/cloudinary/cloudinary.service';
 
 @Injectable()
 export class TaskService {
@@ -21,7 +29,9 @@ export class TaskService {
     @InjectModel(OneTask.name) private readonly _oneTask: Model<ManyTask>,
     @InjectModel(ManyTask.name) private readonly _manyTask: Model<OneTask>,
     @InjectModel(User.name) private readonly _userModel: Model<User>,
+    private readonly _cloudinaryService: CloudinaryService,
   ) {}
+
   home(): String {
     return 'Task Page';
   }
@@ -97,9 +107,9 @@ export class TaskService {
   async deleteTask(params: ObjectIdDTO): Promise<object> {
     const { id } = params;
 
-    const { deletedCount } = await this._taskModel.deleteOne({ _id: id });
+    const task = await this._taskModel.findByIdAndDelete(id);
 
-    if (!deletedCount) throw new NotFoundException('invalid task id');
+    if (!task) throw new NotFoundException('invalid task id');
 
     return {
       message: 'Task Deleted',
@@ -107,16 +117,94 @@ export class TaskService {
   }
 
   async getAllTaskToOne(params: ObjectIdDTO) {
-    const { id } = params; // user id
+    const { id } = params; // userId
 
     const tasks = await this._taskModel.find({
-      $or: [{ assignTo: id }, { assignTo: { $elemMatch: { $eq: id } } }],
+      $or: [{ assignTo: id }, { assignTo: { $elemMatch: { $eq: id } } }], // oneTask or manyTask
     });
+
     if (!tasks.length) throw new NotFoundException('no tasks found');
 
     return {
       message: 'done',
       tasks,
+    };
+  }
+
+  async getAllLateTask(req: any) {
+    const { id } = req.user;
+    const currentDate = new Date();
+
+    const tasks = await this._taskModel.find({
+      deadline: { $lt: currentDate },
+      $or: [{ assignTo: id }, { assignTo: { $elemMatch: { $eq: id } } }],
+    });
+
+    if (!tasks.length) throw new NotFoundException('no task found');
+
+    return {
+      message: 'done',
+      tasks,
+    };
+  }
+
+  // todo upload on cloud without upload on local $$ test the methods
+  async uploadAttachment(req: any) {
+    const file: Express.Multer.File = req.file;
+    const id: string = req.params.id; // task id
+    const isTask: TtaskModel = await this._taskModel.findById(id);
+
+    if (!isTask) throw new NotFoundException('task id not found');
+
+    const { public_id, secure_url } = await this._cloudinaryService.uploadFile({
+      file: file,
+      folder: `${cloudinaryFolderPath}/${id}`,
+    });
+
+    const model =
+      isTask.type == taskTypes.oneTask ? this._oneTask : this._manyTask;
+
+    await model.updateOne(
+      { _id: id },
+      { $push: { attachments: { public_id, secure_url } } },
+    );
+
+    return {
+      message: 'uploaded',
+    };
+  }
+
+  //todo check for the model type to handle findoneandupdate
+  async deleteAttachment(req: any): Promise<object> {
+    const { id } = req.params;
+    const { public_id } = req.body;
+
+    const isTask: TtaskModel = await this._taskModel.findById(id);
+    if (!isTask) throw new NotFoundException('task id not found');
+
+    // delete image from cloud
+
+    let model: any =
+      isTask.type == taskTypes.oneTask ? this._oneTask : this._manyTask;
+
+    // delete attach from db
+    const { attachments } = await model.findByIdAndUpdate(
+      id,
+      {
+        $pull: { attachments: { public_id } },
+      },
+      {
+        new: true,
+      },
+    );
+
+    if (isTask.attachments.length === attachments.length)
+      throw new BadRequestException('public_id not found in the DB');
+
+    await this._cloudinaryService.deleteFile(public_id);
+
+    return {
+      message: 'deleted',
     };
   }
 
